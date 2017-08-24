a5423b747aeb5dbf841b1f20a0aba820cb151319
Olivier Bellone <ob@stripe.com>
2017-08-24 15:07:15 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Stripe webhook Java 7 example
+
+This is a simple example project illustrating how to implement a [Stripe webhooks endpoint](https://stripe.com/docs/webhooks).
+
+It will also [verify the events](https://stripe.com/docs/webhooks#verifying-events) by using [Stripe's Java library](https://github.com/stripe/stripe-java) to check the events' signatures.
+
+*Note:* If you're using Java 8 or higher, you should look at the [example for Java 8](https://github.com/ob-stripe/example-stripe-java8-webhook) instead.
+
+## Use with Heroku
+
+Head to your [webhooks](https://dashboard.stripe.com/account/webhooks) settings and create an endpoint with any URL (you'll change it later). Click on the button to reveal the signing secret.
+
+Then deploy the project on your Heroku account:
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+When deploying, you'll be prompted for the webhook signing secret.
+
+Once the app is running on Heroku, head back to the webhooks settings and update the endpoint's URL with your app's URL followed by `/webhook`. E.g. if the URL of the Heroku app is `https://thawing-crag-63114.herokuapp.com`, set the endpoint's URL to be `https://thawing-crag-63114.herokuapp.com/webhook`.
+
+You can then create test events and view the logs of your Heroku app to check that the events were correctly received and verified.
+
+## Use as a standalone project
+
+### Requirements
+
+- Java 1.7 or later
+- Maven
+
+### Instructions
+
+Clone the repository:
+
+```bash
+git clone https://github.com/ob-stripe/example-stripe-java7-webhook.git
+```
+
+Compile the project:
+
+```bash
+mvn compile
+```
+
+Head to your [webhooks](https://dashboard.stripe.com/account/webhooks) settings and add a test webhook endpoint with URL set to your server's URL followed by `/webhook`. The server runs on port 4567 by default (you can use the `PORT` environment variable to change this), so for instance if your server is accessible at `myserver.com`, set the endpoint's URL to be `http://myserver.com:4567/webhook`.
+
+If your server is not directly visible from the internet, you can use [ngrok](https://ngrok.com/) to create a tunnel:
+
+```bash
+ngrok http 4567
+```
+
+then set the endpoint's URL to the one provided by ngrok followed by `/webhook`, e.g. `http://87fe9a4d.ngrok.io/webhook`.
+
+Once the endpoint is created, you can reveal the signing secret and use its value to run the project:
+
+```bash
+STRIPE_ENDPOINT_SECRET=whsec_... \
+mvn exec:java
+```
+
+You can then create test events on your Stripe account and view the logs of the app to check that the events were correctly received and verified.
---
+++ src/main/java/com/stripe/ExampleStripeJava7Webhook.java
@@ -0,0 +1,88 @@
+package com.stripe;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static spark.Spark.post;
+import static spark.Spark.setPort;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.http.HttpStatus;
+
+import com.google.gson.JsonSyntaxException;
+
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.model.Event;
+import com.stripe.net.Webhook;
+
+
+public final class ExampleStripeJava7Webhook {
+    private static final Logger LOGGER =
+        LoggerFactory.getLogger(ExampleStripeJava7Webhook.class);
+
+    private ExampleStripeJava7Webhook() { }
+
+    public static void main(final String[] args) throws IOException {
+        Map<String, String> env = System.getenv();
+
+        // Retrieve webhook endpoint's signing secret
+        final String endpointSecret = env.get("STRIPE_ENDPOINT_SECRET");
+
+        // Set the webserver's port, if necessary
+        if (env.containsKey("PORT")) {
+            setPort(Integer.parseInt(env.get("PORT")));
+        }
+
+        post(new Route("/webhook") {
+            @Override
+            public Object handle(final Request request,
+                                 final Response response) {
+                String payload = request.body();
+                String sigHeader = request.headers("Stripe-Signature");
+                Event event = null;
+
+                try {
+                    // Retrieve the request's body and parse it as JSON
+                    event = Webhook.constructEvent(
+                        payload, sigHeader, endpointSecret
+                    );
+                } catch (JsonSyntaxException e) {
+                    String error = "Error: invalid JSON payload ("
+                                 + e.getMessage() + ")";
+                    LOGGER.warn(error);
+                    response.status(HttpStatus.SC_BAD_REQUEST);
+                    return error;
+                } catch (SignatureVerificationException e) {
+                    String error = "Error: signature verification failed ("
+                                 + e.getMessage() + ")";
+                    LOGGER.warn(error);
+                    response.status(HttpStatus.SC_BAD_REQUEST);
+                    return error;
+                } catch (Exception e) {
+                    String error = "Error: unexpected exception "
+                                 + e.getClass().getName() + " ("
+                                 + e.getMessage() + ")\n";
+                    LOGGER.warn(error);
+                    response.status(HttpStatus.SC_BAD_REQUEST);
+                    return error;
+                }
+
+                LOGGER.info("Received event: " + event.getId()
+                            + ", type: " + event.getType()
+                            + ", account: " + event.getAccount());
+
+                // Do something with event
+
+                // Reply with 200 status code to acknowledge receipt of the
+                // webhook
+                response.status(HttpStatus.SC_OK);
+                return "";
+            }
+        });
+    }
+}
